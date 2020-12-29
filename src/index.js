7390510d926e2c4560d26cd4de61b13bec865400
Geovanny Andres Gonzalez <ga.gonzalezr@uniandes.edu.co>
2020-12-28 20:41:18 -0500

    Version 1 - Servidor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+/**
+ * Implementación del servidor WebSocket
+ * Infraestructura Computacional
+ * Universidad de los Andes
+ *
+ * Version 1.0
+ * @author Geovanny González-Rodríguez
+ * @license MIT
+ * @since Diciembre 2020
+ */
+
+// Cargar las variables de entorno
 import "dotenv/config";
 
-console.log(`Hola ${process.env.PIPO}`);
+// Cargar la libreria de WebSockets
+import * as WebSocket from "ws";
+
+// Instancia del servidor
+const wsServer = new WebSocket.Server({
+  port: 3000,
+});
+
+// A continuación vamos a definir algunas funciones para manejar los eventos
+// del servidor. Luego vamos a configurarlas
+
+/**
+ * Procesa la petición del nuevo cliente entrante
+ * @param {*} client WebSocket Client - Cliente de una nueva conexión
+ * @param {*} request Petición realizada al servidor.
+ */
+const clientReceived = (client, request) => {
+  // Datos del cliente
+  console.log(`Dirección IP del Cliente: ${request.socket.remoteAddress}`);
+
+  // Respuesta al evento de un nuevo mensaje
+  client.on("message", (messageData) => {
+    console.log(`Mensaje recibido por parte del cliente: ${messageData}`);
+    const messageToSend = messageReceived(messageData);
+    client.send(messageToSend);
+  });
+};
+
+/**
+ * Procesa el mensaje recibido por un cliente de
+ * acuerdo al protocolo establecido
+ * @param {*} data Mensaje enviado por el cliente
+ */
+const messageReceived = (data) => {
+  return "Hola Mundo por parte del Servidor";
+};
+
+// Configuración de las funciones de eventos.
+wsServer.addListener("connection", clientReceived);
